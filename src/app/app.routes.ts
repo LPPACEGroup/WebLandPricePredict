eea71658a81b55c332a218e351871b6f80f2cfa9
Nattaphon <nattaphonnuayen@gmail.com>
2024-04-08 13:11:09 +0700

    add tailwind and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { CalculatorComponent } from './page/calculator/calculator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   path: 'Home', component: HomeComponent },
+    { path: 'Calculator', component: CalculatorComponent },
+    {path:'**',component:HomeComponent}];
+    
