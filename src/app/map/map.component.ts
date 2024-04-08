9a62de967c42630eac232c65399d955e835fc7d1
Nattaphon <nattaphonnuayen@gmail.com>
2024-04-08 11:28:52 +0700

    ss

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,19 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
-import { OSMDataService } from '../osm-data.service'; // Import your service for handling OSM data requests
+import 'leaflet-draw';
+import 'leaflet-draw/dist/leaflet.draw';
 
+import { OSMDataService } from '../osm-data.service'; // Import your service for handling OSM data requests
+interface OverpassElement {
+  type: string;
+  id: number;
+  lat: number;
+  lon: number;
+  tags: {
+    [key: string]: string;
+    name: string;
+  };
+}
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -12,6 +24,7 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   constructor(private osmDataService: OSMDataService) {} // Inject your service here
 
+  
   private initMap(): void {
     this.map = L.map('map', {
       center: [ 13.7563, 100.5018 ],
@@ -23,29 +36,62 @@ export class MapComponent implements OnInit, AfterViewInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
+
     tiles.addTo(this.map);
     this.map.on('click', (event: any) => {
       const latitude = event.latlng.lat;
       const longitude = event.latlng.lng;
-      console.log('Latitude:', latitude, 'Longitude:', longitude);
-      
-      // Call a method to handle the query using the latitude and longitude
-      // this.queryOSMData(latitude, longitude);
-      // this.address(latitude, longitude)
-      this.addMarker(latitude,longitude)
+
     });
+    // เป็นtoolที่ใช้ draw พวก polygon ไม่ก็ marker
+    var drawnItems = new L.FeatureGroup();
+    const drawControl = new L.Control.Draw(
+      {
+        draw: {
+          polygon:false,
+          marker:{
+
+          },
+          circle:false,
+          rectangle:false,
+          circlemarker:false,
+          polyline:false
+        },
+        edit: {
+            featureGroup: drawnItems,
+        }
+    });
+    this.map.addControl(drawControl);
+
+    // event เกิดเมื่อมีการ draw เกิดขึ้น
+    this.map.on(L.Draw.Event.CREATED, (event: any) => {
+      const layer = event.layer;
+      // ทำการเอาพิกัดมา
+      const center = layer.getLatLng();
+      const latitude = center['lat']
+      const longitude = center['lng']
+      // draw circle ตรงที่add พิกัด 
+      L.circle([latitude,longitude], {radius:5000}).addTo(this.map);
+      // querry
+      // this.queryOSMData(latitude, longitude);
+      // ทำการแปลง lat lng เป็นที่อยู่
+      // this.address(latitude, longitude)
+      this.map.addLayer(layer);
+      this.fetchDataFromOverpass(latitude,longitude)
+    });
+
   }
 
   private queryOSMData(latitude: number, longitude: number): void {
     // Call your OSM data service to fetch data based on the clicked coordinates
-    this.osmDataService.getOSMData(latitude, longitude, 5000) // 5 km radius, adjust as needed
+    this.osmDataService.getOSMData(latitude, longitude, 3000) // 5 km radius, adjust as needed
       .subscribe(data => {
         console.log('OSM Data:', data);
       });
   }
 
   private addMarker(latitude: number, longitude: number): void {
-    const marker = L.marker([latitude, longitude]).addTo(this.map);
+    L.marker([latitude, longitude],).addTo(this.map);
   }
 
   private address(latitude: number, longitude: number): void {
@@ -64,5 +110,31 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.initMap();
+  }
+
+  
+
+  private fetchDataFromOverpass(latitude: number, longitude: number): void {
+    const overpassUrl = 'https://overpass-api.de/api/interpreter';
+    // เลือก tag กับระยะ (เมตร ในที่นี้เลือก restaurant)
+    const overpassQuery = `[out:json];
+                           node(around:${5000}, ${latitude}, ${longitude})["amenity"="restaurant"];
+                           out;`;
+
+    fetch(overpassUrl, {
+      method: 'POST',
+      body: overpassQuery
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data && data.elements) {
+        // เอา มาแค่ tag name ถ้าจะเอาอย่างอื่นอ่าจจะต้องใช้อย่างอื่น
+        const restaurantNames = data.elements.map((element: OverpassElement) => element.tags.name|| 'Restaurant');
+        console.log(restaurantNames);  // Print restaurant names to console log
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
   }
 }
