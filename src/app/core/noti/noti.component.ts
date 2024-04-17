57d807c8205c8802d33513ab06f875f36f86a433
Nattaphon <nattaphonnuayen@gmail.com>
2024-04-17 13:03:01 +0700

    change to daisyUI but not clean up yet

---
+++ src/app/core/noti/noti.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-noti',
+  standalone: true,
+  imports: [],
+  templateUrl: './noti.component.html',
+  styleUrl: './noti.component.css'
+})
+export class NotiComponent {
+
+}
