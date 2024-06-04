77cd15d1a0ad773ad323e70582e7d874a7496c71
Nattaphon <nattaphonnuayen@gmail.com>
2024-06-04 22:39:25 +0700

    add dashboard

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { LineChartComponent } from '../../core/line-chart/line-chart.component';
+
+
+import { dataSeries } from "./data-series";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [LineChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  public data1: { date: string, value: number }[] = [];
+  public data2: { date: string, value: number }[] = [];
 
+  constructor() {
+    // ts2 คือเวลา
+    let ts2 = 1484418600000;
+    let dates = [];
+    for (let i = 0; i < 120; i++) {
+      ts2 = ts2 + 86400000;
+      dates.push({ date: new Date(ts2).toISOString().split('T')[0], value: dataSeries[1][i].value });
+    }
+    this.data1 = dates;
+    this.data2 = dates;
+
+  }
+
+  
 }
