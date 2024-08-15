c77982bd7097cbcb74c0df1e49ea7a9923e7d840
SPiCYwant2quit <88331274+SPiCYnam@users.noreply.github.com>
2024-08-15 20:31:55 +0700

    Update dependencies and config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { CalculatorComponent } from './page/calculator/calculator.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { MonitorComponent } from './page/monitor/monitor.component';
+import { QuestionComponent } from './page/question/question.component';
+import { ContactPageComponent } from './page/contact-page/contact-page.component';
+import { ProfilePageComponent } from './page/profile-page/profile-page.component';
+import { SubscriptionPageComponent } from './page/subscription-page/subscription-page.component';
+import { MapPageComponent } from './page/map-page/map-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   path: 'Home', component: HomeComponent },
+    { path: 'Map', component: MapPageComponent },
+    { path: 'Dashboard', component: DashboardComponent },
+    { path: 'Monitor', component: MonitorComponent },
+    { path: 'Calculator', component: CalculatorComponent },
+    { path: 'Question', component: QuestionComponent },
+    { path: 'Contact', component: ContactPageComponent },
+    { path: 'Profile', component: ProfilePageComponent },
+    { path: 'Subscription', component: SubscriptionPageComponent },
+
+    {path:'**',component:HomeComponent},
+];
+    
