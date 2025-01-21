1bb130bee88f8cad02b62e5274c34db97752ef09
Nattaphon <nattaphonnuayen@gmail.com>
2025-01-21 21:58:52 +0700

    add admin blog managing table

---
+++ src/app/service/Blog/blog.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Blog } from 'model/blog.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+
+  apiURL = 'http://localhost:8000/api';
+  constructor(private http: HttpClient) {
+   }
+
+   getBlogs(): Observable<any> {
+    return this.http.get(`${this.apiURL}/blog`, { withCredentials: true });
+   }
+
+   createBlog(blog:Blog): Observable<any> {
+    return this.http.post(`${this.apiURL}/blog`, blog, { withCredentials: true });
+   }
+
+    deleteBlog(id:number): Observable<any> {
+      return this.http.delete(`${this.apiURL}/blog/${id}`, { withCredentials: true });
+    }
+
+    updateBlog(blog_id:number,blog:Blog): Observable<any> {
+      return this.http.put(`${this.apiURL}/blog/${blog_id}`, blog, { withCredentials: true });
+    }
+
+}
