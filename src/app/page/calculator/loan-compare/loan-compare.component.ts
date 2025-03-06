ceadf0250b061cd5afbb58f4be313bf01790df9f
SPiCYwant2quit <88331274+SPiCYnam@users.noreply.github.com>
2025-03-06 13:48:29 +0700

    feat: refactor loan calculator components; add new calculations and styles for improved functionality and user experience

--- src/app/page/calculator/loan-compare/loan-compare.component.ts
+++ src/app/page/calculator/loan-compare/loan-compare.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-loan-compare',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
+  providers: [
+    DecimalPipe
+  ],
   templateUrl: './loan-compare.component.html',
   styleUrl: './loan-compare.component.css'
 })
 export class LoanCompareComponent {
+  loanAmount!: number;
+  loanTerm1!: number;
+  loanTerm2!: number;
+  interest1!: number;
+  interest2!: number;
 
+  monthAmount1!: number;
+  monthAmount2!: number;
+  totalInterest1!: number;
+  totalInterest2!: number;
+  monthInterest1!: number;
+  monthInterest2!: number;
+
+  monthlyPayment1!: number;
+  monthlyPayment2!: number;
+
+  calculate() {
+    // 1st loan
+    const principalAmount = this.loanAmount;
+    const interest1 = this.interest1 / 100;
+    this.monthAmount1 = this.loanTerm1 * 12;
+
+    this.totalInterest1 = principalAmount*interest1*this.loanTerm1;
+    this.monthInterest1 = this.totalInterest1/this.monthAmount1;
+
+    const principalPerMonth1 = principalAmount / this.monthAmount1;
+    this.monthlyPayment1 = principalPerMonth1 + this.monthInterest1;
+
+    // 2nd loan
+
+    const interest2 = this.interest2 / 100;
+    this.monthAmount2 = this.loanTerm2 * 12;
+
+    this.totalInterest2 = principalAmount*interest2*this.loanTerm1;
+    this.monthInterest2 = this.totalInterest2/this.monthAmount1;
+
+    const principalPerMonth2 = principalAmount / this.monthAmount2;
+    this.monthlyPayment2 = principalPerMonth2 + this.monthInterest2;
+
+}
 }
--- src/app/page/calculator/loan-maximum/loan-maximum.component.ts
+++ src/app/page/calculator/loan-maximum/loan-maximum.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe, CommonModule  } from '@angular/common';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-loan-maximum',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
+  providers: [
+    DecimalPipe
+  ],
   templateUrl: './loan-maximum.component.html',
   styleUrl: './loan-maximum.component.css'
 })
 export class LoanMaximumComponent {
+  monthlyIncome!: number;
+  loanTerm!: number;
+  interestRate!: number;
+  maximumLoanPercentage!: number;
+  monthlyDebt!: number;
+  maximumLoanAmount!: number;
+  newMonthlyPayment!: number;
+  averageLoanRatio = 150;
+calculate() {
+    // const monthlyIncome = parseFloat(this.monthlyIncome);
+    // const loanTerm = parseFloat(this.loanTerm);
+    // const interestRate = parseFloat(this.interestRate) / 100 / 12;
+    // const maximumLoanPercentage = parseFloat(this.maximumLoanPercentage) / 100;
+    // const monthlyDebt = parseFloat(this.monthlyDebt);
 
+    const newInterestRate = this.interestRate / 100 / 12;
+    const newMaximumLoanPercentage = this.maximumLoanPercentage / 100;
+    const numberOfPayments = this.loanTerm * 12;
+
+    this.maximumLoanAmount = ((this.monthlyIncome - this.monthlyDebt  ) * newMaximumLoanPercentage)* this.averageLoanRatio;
+
+    this.newMonthlyPayment = this.maximumLoanAmount * newInterestRate / (1 - Math.pow(1 + newInterestRate, -numberOfPayments));
+
+  }
 }
+
